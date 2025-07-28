98991a38328d2b2b63f050fc63080e4b8aff0f25
StephenDTurner <frawdo@icloud.com>
2025-07-28 20:32:39 +0100

    Default to Pavlicek

--- src/lib/stores/algorithm.ts
+++ src/lib/stores/algorithm.ts
@@ -2,7 +2,7 @@ import { Algorithm } from '$lib/types/bridge';
 import { writable } from 'svelte/store';
 
 
-export const algorithm = writable<Algorithm>(Algorithm.Random);
+export const algorithm = writable<Algorithm>(Algorithm.Pavlicek);
 
 export function setAlgorithm(algo: Algorithm) {
   algorithm.set(algo);
